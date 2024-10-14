4483d32c7c6fc62c621ccfd96e6221f956bec364
TomasConti02 <tomasconticestino@gmail.com>
2024-10-14 14:37:56 +0200

    Update MatrixSum.cu

--- MatrixSum.cu
+++ MatrixSum.cu
@@ -3,14 +3,14 @@
 #include <sys/time.h>
 #include <math.h> // For fabs() function
 
-#define CHECK(call) { 
-    const cudaError_t error = call; 
-    if (error != cudaSuccess) { 
-        printf("Error: %s:%d, ", __FILE__, __LINE__); 
-        printf("code: %d, reason: %s\n", error, cudaGetErrorString(error)); 
-        exit(1); 
-    } 
-}
+#define CHECK(call) { \
+    const cudaError_t error = call; \
+    if (error != cudaSuccess) { \
+        printf("Error: %s:%d, ", __FILE__, __LINE__); \
+        printf("code: %d, reason: %s\n", error, cudaGetErrorString(error)); \
+        exit(1); \
+    } \
+} \
 
 // Function to measure time in seconds
 double cpuSecond() {
