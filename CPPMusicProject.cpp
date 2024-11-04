38493f5790cc9cda062cf667e5a2afc0963c2c85
Tomas Conti <tomasconticestino@gmail.com>
2024-11-04 15:59:19 +0100

    Update CPPMusicProject.cpp

--- CPPMusicProject.cpp
+++ CPPMusicProject.cpp
@@ -3,21 +3,35 @@
 #include <vector>
 
 struct WAVHeader {
-    char chunkID[4];
-    uint32_t chunkSize;
-    char format[4];
+    char chunkID[4]; //type of file
+    uint32_t chunkSize; //file size 
+    char format[4]; //format file, our case we have WAVE for file WAV
     char subchunk1ID[4];
     uint32_t subchunk1Size;
     uint16_t audioFormat;
-    uint16_t numChannels;
-    uint32_t sampleRate;
-    uint32_t byteRate;
-    uint16_t blockAlign;
+    uint16_t numChannels; //channel number
+    uint32_t sampleRate; //Hz 44100
+    uint32_t byteRate; //number of byte every audio second
+    uint16_t blockAlign; //number of byte for audio sample
     uint16_t bitsPerSample;
     char subchunk2ID[4];
     uint32_t subchunk2Size;
 };
-
+void printWAVHeader(const WAVHeader& header) {
+    std::cout << "chunkID: " << std::string(header.chunkID, 4) << std::endl;
+    std::cout << "chunkSize: " << header.chunkSize << std::endl;
+    std::cout << "format: " << std::string(header.format, 4) << std::endl;
+    std::cout << "subchunk1ID: " << std::string(header.subchunk1ID, 4) << std::endl;
+    std::cout << "subchunk1Size: " << header.subchunk1Size << std::endl;
+    std::cout << "audioFormat: " << header.audioFormat << std::endl;
+    std::cout << "numChannels: " << header.numChannels << std::endl;
+    std::cout << "sampleRate: " << header.sampleRate << " Hz" << std::endl;
+    std::cout << "byteRate: " << header.byteRate << " bytes/sec" << std::endl;
+    std::cout << "blockAlign: " << header.blockAlign << " bytes" << std::endl;
+    std::cout << "bitsPerSample: " << header.bitsPerSample << " bits" << std::endl;
+    std::cout << "subchunk2ID: " << std::string(header.subchunk2ID, 4) << std::endl;
+    std::cout << "subchunk2Size: " << header.subchunk2Size << " bytes" << std::endl;
+}
 void printSampleValues(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
     if (!file) {
@@ -27,8 +41,8 @@ void printSampleValues(const std::string& filename) {
 
     WAVHeader header;
     file.read(reinterpret_cast<char*>(&header), sizeof(header));
-
-    std::cout << "Numero di canali: " << header.numChannels
+    printWAVHeader(header);
+    std::cout << "\nNumero di canali: " << header.numChannels
               << "\nFrequenza di campionamento: " << header.sampleRate << " Hz"
               << "\nBit per campione: " << header.bitsPerSample << "\n";
 
@@ -40,7 +54,7 @@ void printSampleValues(const std::string& filename) {
     for (int i = 0; i < numSamples; ++i) {
         file.read(sampleBuffer.data(), bytesPerSample);
         int sampleValue = *reinterpret_cast<int16_t*>(sampleBuffer.data());
-        std::cout << "Campione " << i + 1 << ": " << sampleValue << '\n';
+        //std::cout << "Campione " << i + 1 << ": " << sampleValue << '\n';
     }
 }
 
