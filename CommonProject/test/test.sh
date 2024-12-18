ed21264fcdd1b934f617ea300a2df064028ec363
Jacopo <96496259+gosuilgiallo@users.noreply.github.com>
2024-12-18 17:05:38 +0100

    Update test.sh

--- CommonProject/test/test.sh
+++ CommonProject/test/test.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# usage example: ./test.sh iir.cpp
+# for iir.cpp -> ./test.sh iir.cpp
+# for the others -> ./test.sh <filename>.cpp -lsndfile -fopenmp -lfftw3f
 
 if [ -z "$1" ]; then
-    echo "You must specify the name of the c++ file."
-    echo "Usage: $0 <src_file.cpp>"
+    echo "You must specify the name of the C++ file."
+    echo "Usage: $0 <src_file.cpp> [additional_compile_options]"
     exit 1
 fi
 
 # vars
 SOURCE_FILE="$1"
 EXECUTABLE="${SOURCE_FILE%.cpp}"
+shift 
+COMPILE_OPTIONS="$@" # compile options
 cd ..
 LOG_FILE="./test/${EXECUTABLE}_runs.txt"
-NUM_RUNS=10
+NUM_RUNS=20
 
 # cleans log file
 > $LOG_FILE
-
 
 # loops for each opt level
 for OPT_LEVEL in 0 1 2 3; do
     echo "Compiling in -O$OPT_LEVEL..." | tee -a $LOG_FILE
 
     # compiling
-    g++ -o $EXECUTABLE $SOURCE_FILE -O$OPT_LEVEL -march=native -msse2
+    g++ -o $EXECUTABLE $SOURCE_FILE -O$OPT_LEVEL -march=native -msse2 $COMPILE_OPTIONS
 
     # execution
     for ((i = 1; i <= NUM_RUNS; i++)); do
@@ -40,4 +42,4 @@ cd ./test
 
 FILE_NAME="${LOG_FILE#./test/}"
 
-python3 speedup_analysis.py "$FILE_NAME"
+python3 speedup_analysis.py "$FILE_NAME" >> $FILE_NAME
